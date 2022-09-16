9231fd9cfacdcccab36341b4562a36c1f8646cd0
Stella Cannefax <stella@mystenlabs.com>
2022-09-16 11:07:15 -0500

    add getEventByModule script

---
+++ Assets/Scripts/Components/Test/Event/GetEventsByModuleTest.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(SuiClient))]
+public class GetEventsByModuleTest : MonoBehaviour
+{
+    Task<object[]> Task;
+
+    // substitute your struct name here
+    public string Package = "0x2";
+    public string Module = "devnet_nft";
+    
+    [Range(0, 100)]
+    public uint Count = 50;
+
+    void Start()
+    {
+        var client = gameObject.GetComponent<SuiClient>();
+        Task = client.Rpc.GetEventsByModule(Package, Module, Count);
+    }
+
+    void Update()
+    {
+        if (Task is not {IsCompleted: true}) 
+            return;
+        
+        var result = Task.Result;
+        var resLines = string.Join(",\n", (IEnumerable<object>) result);
+        var resultText = $"[\n{resLines}\n]";
+        
+        Debug.Log("get events by module result:  " + resultText);
+        Task = null;
+    }
+}
