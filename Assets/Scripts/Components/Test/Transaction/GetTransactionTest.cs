5a385aff090c189af672d31366b8ceb77378d765
Stella Cannefax <stella@mystenlabs.com>
2022-09-16 14:32:31 -0500

    improve GetTransactionTest logging

--- Assets/Scripts/Components/Test/Transaction/GetTransactionTest.cs
+++ Assets/Scripts/Components/Test/Transaction/GetTransactionTest.cs
@@ -28,7 +28,15 @@ public class GetTransactionTest : MonoBehaviour
         Debug.Log("get tx - result:  " + result);
         Debug.Log("get tx, time: " + result.Timestamp);
         Debug.Log("get tx, parsed: " + result.ParsedData);
-        Debug.Log("get tx - effects: " + result.Effects);
+        
+        //Debug.Log("get tx - effects: " + result.Effects);
+        Debug.Log("get tx - effects.status.status: " + result.Effects.Status.Status);
+        Debug.Log("get tx - effects.transactionDigest: " + result.Effects.TransactionDigest);
+        Debug.Log("get tx - effects.gasUsed: " + result.Effects.GasUsed);
+        Debug.Log("get tx - effects.gasObject: " + result.Effects.GasObject);
+        Debug.Log("get tx - effects.created: " + result.Effects.Created);
+        Debug.Log("get tx - effects.mutated: " + result.Effects.Mutated);
+        Debug.Log("get tx - effects.events: " + result.Effects.Events);
         
         Debug.Log("get tx - cert TYPE:  " + result.Certificate.GetType());
         Debug.Log("get tx - cert:  " + result.Certificate);
@@ -36,14 +44,11 @@ public class GetTransactionTest : MonoBehaviour
         Debug.Log("get tx - cert.data:  " + ((JObject)result.Certificate)["data"]);
 
         /*
-
         var cert = result.Certificate;
         Debug.Log("get tx - cert, digest: " + cert.TransactionDigest);  
         Debug.Log("get tx - cert, signature: " + cert.Signature);
         Debug.Log("get tx - cert, data: " + cert.Data);
         Debug.Log("get tx - cert, authSignInfo:\n" + cert.AuthoritySignInfo);
-        
-
         */
 
         GetTransactionTask = null;
